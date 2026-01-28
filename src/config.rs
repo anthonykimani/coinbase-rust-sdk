1051e8852bcc3995a92271fa9e3a8e139eb527b5
anthony <kimaniantoni@gmail.com>
2026-01-28 08:51:57 +0300

    feat: adds default base_path and alternative with_base_path method

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+const DEFAULT_BASE_PATH: &str = "https://api.cdp.coinbase.com/platform";
+
 #[derive(Debug, Clone)]
 pub struct Config {
     pub api_key_id: String,
@@ -8,13 +10,17 @@ pub struct Config {
 impl Config {
     pub fn new(
         api_key_id: impl Into<String>,
-        api_key_secret: impl Into<String>,
-        base_path: impl Into<String>,
+        api_key_secret: impl Into<String>
     ) -> Self {
         Self {
             api_key_id: api_key_id.into(),
             api_key_secret: api_key_secret.into(),
-            base_path: base_path.into()
+            base_path: DEFAULT_BASE_PATH.to_string()
         }
     }
+
+    pub fn with_base_path(self, base_path: impl Into<String>) -> Self {
+        Self { base_path: base_path.into(), ..self }
+    }
 }
+
